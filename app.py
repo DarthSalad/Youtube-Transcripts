9d949cf60b81667f44ae289252561ffd90e6ad33
DarthSalad <piyushmishra965@gmail.com>
2021-08-13 20:56:18 +0530

    all fucntions

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import datetime
 from flask import Flask
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
@@ -11,15 +12,31 @@ app.route('/time', methods=['GET'])
 def get_time():
     return str(datetime.datetime.now())
 
-def show_transcripts(str):
-    import requests
-    import json
-    from youtube_transcript_api import YouTubeTranscriptApi
+def summarized_sub(script):
+    model = T5ForConditionalGeneration.from_pretrained("t5-base")
+    tokenizer = T5Tokenizer.from_pretrained("t5-base")
+    inputs = tokenizer.encode("summarize: " + script, return_tensors="pt", max_length=2048, truncation=True)
+    outputs = model.generate(
+                inputs, 
+                max_length=150, 
+                min_length=40, 
+                length_penalty=2.0, 
+                num_beams=4, 
+                early_stopping=True
+                )
+    print(tokenizer.decode(outputs[0]))
 
-    sub = YouTubeTranscriptApi.get_transcript('YtIw3wqHeNY')
+def show_transcripts(video_id):
+    summary = ""
+    sub = YouTubeTranscriptApi.get_transcript(video_id)
     for params in sub:
-        print(params['text'], end=' ')
+        # print(params['text'], end=' ')
+        summary += params['text']
+        summary += " "
+    # print(summary)
+    summarized_sub(summary)
 
+show_transcripts('YtIw3wqHeNY')
 
 if __name__ == '__main__':
     app.run()
