67df53f15a2c8d4e6b631f68d0ac38590c2794b0
Piyush Mishra <piyushmishra965@gmail.com>
2021-09-13 19:29:45 +0530

    added files for extension popup

--- app.py
+++ app.py
@@ -1,36 +1,17 @@
 import datetime
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
-@app.route('/summarize/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/')
-def index_page():
+@app.route('/test')
+def test():
     return "test success"
 
-@app.route('/time', methods=['GET'])
-def get_time():
-    return str(datetime.datetime.now())
+@app.route('/test2')
+def test2():
+    return redirect("https://github.com/DarthSalad")
 
 def summarized_sub(script):
     model = T5ForConditionalGeneration.from_pretrained("t5-base")
@@ -44,11 +25,14 @@ def summarized_sub(script):
                 num_beams=4, 
                 early_stopping=True
                 )
-    print(tokenizer.decode(outputs[0]))
+    # print(tokenizer.decode(outputs[0]))
+    return tokenizer.decode(outputs[0])
 
-def show_transcripts(video_id):
+@app.route('/api/summarize', methods=['GET'])
+def show_transcripts():
+    vid_id = input("Enter the YT link: ")[-11:]
     summary = ""
-    sub = YouTubeTranscriptApi.get_transcript(video_id)
+    sub = YouTubeTranscriptApi.get_transcript(vid_id)
     for params in sub:
         # print(params['text'], end=' ')
         summary += params['text']
@@ -56,7 +40,6 @@ def show_transcripts(video_id):
     # print(summary)
     summarized_sub(summary)
 
-# vid_id = input("Enter the YT link: ")
 # show_transcripts(vid_id[-11:])
 
 if __name__ == '__main__':
